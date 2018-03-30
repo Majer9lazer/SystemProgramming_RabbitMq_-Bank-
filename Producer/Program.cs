bee22810c6efd3f18b95d63e549acbaa669b99eb
СидоренкоЕ <@403_2>
2018-03-30 20:46:54 +0600

    Добавил Регистрацию

--- Producer/Program.cs
+++ Producer/Program.cs
@@ -1,35 +1,62 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Linq;
 using Newtonsoft.Json.Bson;
 using System.Windows;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
 namespace Producer
 {
     class Program
     {
         public static string DocName = "LocalXmlDatabase";
         public static string DocExtension = ".xml";
+
+        private static void FillInDb(ref Db db)
+        {
+            object converted;
+            using (StreamReader str = new StreamReader("Users.json"))
+            {
+                converted = JsonConvert.DeserializeObject(str.ReadToEnd());
+            }
+            JArray arr = (JArray)converted;
+            foreach (JToken t in arr)
+            {
+                Dictionary<IJEnumerable<JToken>, JToken> dd;
+                dd = t.ToDictionary(w => w.Values());
+                var ss = t.ToList();
+                User u = new User();
+                foreach (JToken token in ss)
+                {
+                    string key = token.Value<JProperty>().Name;
+                    string value = token.Value<JProperty>().Value.ToString();
+                    switch (key)
+                    {
+                        case "id": { u.UserId = int.Parse(value); } break;
+                        case "UserName": { u.UserName = value; } break;
+                        case "Age": { u.Age = int.Parse(value); } break;
+                        case "UserMail": { u.UserMail = value; } break;
+                        case "UserGuid": { u.UserGuid = (value); } break;
+                        case "UserNumber": { u.UserNumber = value; } break;
+                    }
+
+                }
+                db.Add(u);
+            }
+        }
         static void Main(string[] args)
         {
 
             Db a = new Db(DocName + DocExtension);
 
-            User Egor = new User()
-            {
-                UserGuid = Guid.NewGuid(),
-                Age = 18,
-                UserMail = "sidorenkoegor1999@mail.ru",
-                UserName = "Egor",
-                UserNumber = "+77051642833"
-            };
-            //XDocu
-            a.Add(Egor);
-            //xdoc.Save(AppDomain.CurrentDomain.BaseDirectory + DocName + DocExtension);
             Console.WriteLine("Всё прошло успешно");
+
 
             Console.ReadLine();
         }
@@ -41,41 +68,30 @@ namespace Producer
                 DbFile = dbFileName;
             }
 
-            private string DbFile { get; set; }
+            private string DbFile { get; }
             public void Add(User u)
             {
                 XDocument xdoc = XDocument.Load(AppDomain.CurrentDomain.BaseDirectory + DocName + DocExtension);
                 var element = xdoc.Element("Users");
-                element?.Add(new XElement("User",
-                    new XAttribute("Id", GetMaxUserId()+1),
-                    new XElement("Name", u.UserName),
-                    new XElement("Age", u.Age),
-                    new XElement("UserMail", u.UserMail),
-                    new XElement("UserNumber", u.UserNumber),
-                    new XElement("UserGuid",u.UserGuid)));
+                if (u.UserId != null)
+                    element?.Add(new XElement("User",
+                        new XAttribute("Id", u.UserId),
+                        new XElement("Name", u.UserName),
+                        new XElement("Age", u.Age),
+                        new XElement("UserMail", u.UserMail),
+                        new XElement("UserNumber", u.UserNumber),
+                        new XElement("UserGuid", u.UserGuid)));
                 xdoc.Save(DbFile);
-            }
-
-
-
-            private int GetMaxUserId()
-            {
-                XDocument xdoc = XDocument.Load(DbFile);
-                int a = Convert.ToInt32(xdoc.Elements().Elements().Max(w => Convert.ToInt32(w.Attribute("Id").Value)));
-                //.Select(s=>new
-                //{
-                //    s.Name,s.Value
-                //});
-                return a;
             }
         }
     }
 
     struct User
     {
+        public int? UserId { get; set; }
         public string UserName { get; set; }
         public int Age { get; set; }
-        public Guid UserGuid { get; set; }
+        public string UserGuid { get; set; }
         public string UserNumber { get; set; }
         public string UserMail { get; set; }
 
---
+++ UserInterface/MainWindow.xaml.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace UserInterface
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            TextBoxMessages = new Dictionary<string, string>()
+            {
+                { "UserNameTextBox","Your Name Here"},
+                { "UserAgeTextBox","Your Age Here"},
+                {"UserMailTextBox","Your Mail Here"},
+                {"UserNumberTextBox","Your Number Here"}
+            };
+            InitializeComponent();
+        }
+
+        public Dictionary<string, string> TextBoxMessages;
+        private void UserNameTextBox_OnGotFocus(object sender, RoutedEventArgs e)
+        {
+
+            if (UserNameTextBox.Text == "Your Name Here")
+            {
+                UserNameTextBox.Text = null;
+                UserNameTextBox.Background = new SolidColorBrush(Colors.White);
+            }
+            else
+            {
+                UserNameTextBox.Background = new SolidColorBrush(Colors.White);
+            }
+
+        }
+
+        private void UserAgeTextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (UserAgeTextBox.Text == "Your Age Here")
+            {
+                UserAgeTextBox.Text = null;
+                UserAgeTextBox.Background = new SolidColorBrush(Colors.White);
+            }
+            else
+            {
+                UserAgeTextBox.Background = new SolidColorBrush(Colors.White);
+            }
+        }
+
+        private void UserMailTextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (UserMailTextBox.Text == "Your Mail Here")
+            {
+                UserMailTextBox.Text = null;
+                UserMailTextBox.Background = new SolidColorBrush(Colors.White);
+            }
+            else
+            {
+                UserMailTextBox.Background = new SolidColorBrush(Colors.White);
+            }
+        }
+
+        private void UserNumberTextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (UserNumberTextBox.Text == "Your Number Here")
+            {
+                UserNumberTextBox.Text = null;
+                UserNumberTextBox.Background = new SolidColorBrush(Colors.White);
+            }
+            else
+            {
+                UserNumberTextBox.Background = new SolidColorBrush(Colors.White);
+            }
+        }
+
+        private void RegistrateButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            TextBox[] arrTextBoxs ={UserNameTextBox,UserAgeTextBox,UserMailTextBox,UserNumberTextBox };
+            foreach (TextBox box in arrTextBoxs)
+            {
+                if (TextBoxMessages.First(f => f.Key == box.Name).Value == box.Text)
+                {
+                    box.Background = new SolidColorBrush(Colors.Red);
+                }
+            }
+
+            if (arrTextBoxs.Any(b => b.Background.ToString() == new SolidColorBrush(Colors.Red).ToString()))
+            {
+                MessageBox.Show("Вы не можете зарегестрироваться так как неправильные поля отмечены красным цветом");
+            }
+            else
+            {
+                MessageBox.Show("Добро мать его пожаловать! , ну почти)) ");
+                foreach (KeyValuePair<string,string> mesage in TextBoxMessages)
+                {
+                    TextBox firstOrDefault = arrTextBoxs.FirstOrDefault(f => f.Name == mesage.Key);
+                    if (firstOrDefault != null && mesage.Key == firstOrDefault.Name)
+                    {
+                        firstOrDefault.Text = mesage.Value;
+                    }
+                }
+            }
+
+
+
+
+        }
+    }
+}
